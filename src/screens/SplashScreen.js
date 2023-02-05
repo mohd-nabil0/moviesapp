91e207659a4b7b80b6a9ecb0d4bdbaceddb1133b
mohd-nabil0 <124254233+mohd-nabil0@users.noreply.github.com>
2023-02-05 17:32:58 +0530

    Added dispatch dependency

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -29,7 +29,7 @@ const SplashScreen = props => {
   useEffect(() => {
     dispatch(getUser());
     dispatch(getLanguage());
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     handleLanguage();
